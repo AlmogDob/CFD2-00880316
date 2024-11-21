bc374eeb8b64bd8c6a0838fa8d16d16e7d7e9385
Almog Dobrescu <dobalmog123@gmail.com>
2024-11-21 18:12:48 +0200

    adding 'create empty dir'

--- Ex0/code/solver.c
+++ Ex0/code/solver.c
@@ -2,6 +2,9 @@
 #include <string.h>
 #include <math.h>
 #include <stdlib.h>
+#include <sys/stat.h>
+#include <dirent.h>
+#include <errno.h>
 
 #define MAXDIR 1000
 #define MAXWORD MAXDIR
@@ -11,20 +14,24 @@
 #define dprintF(expr) do{printf("%d: ", __LINE__); printf(#expr " = %g\n", expr);} while(0)     /* macro for easy debuging*/
 #define dprintD(expr) do{printf("%d: ", __LINE__); printf(#expr " = %g\n", expr);} while(0)     /* macro for easy debuging*/
 
+int create_empty_dir(char *parent_directory);
+
 int main(int argc, char const *argv[])
 {
 /* declerations */
-    char input_dir[MAXDIR], output_dir[MAXDIR], current_word[MAXWORD];
+    char input_fill[MAXDIR], output_dir[MAXDIR], current_word[MAXWORD];
 
-/* getting the input directory and mesh directory*/
+    double *A, *B, *C, *D, *u;
+
+/* getting the input file and output file */
     if (--argc != 2 && argc != 4) {
-        fprintf(stderr, "%s:%d: [Error] not right usage... Usage: main 'input file' 'output file'\n", __FILE__, __LINE__);
+        fprintf(stderr, "%s:%d: [Error] not right usage... Usage: main 'input file' 'output directory'\n", __FILE__, __LINE__);
         return -1;
     }
 
-    strncpy(input_dir, (*(++argv)), MAXDIR);
+    strncpy(input_fill, (*(++argv)), MAXDIR);
 
-    if (input_dir[MAXDIR-1] != '\0') {
+    if (input_fill[MAXDIR-1] != '\0') {
         fprintf(stderr, "%s:%d: [Error] input too long\n", __FILE__, __LINE__);
         return -1;
     }
@@ -35,6 +42,9 @@ int main(int argc, char const *argv[])
         fprintf(stderr, "%s:%d: [Error] input too long\n", __FILE__, __LINE__);
         return -1;
     }
+
+/* creating output directory */
+    create_empty_dir(output_dir);
 
 /*------------------------------------------------------------*/
 /* allocating the matrices */
@@ -55,5 +65,49 @@ int main(int argc, char const *argv[])
 /*------------------------------------------------------------*/
 /* freeing the memory */
 
+    return 0;
+}
+
+/* create empty dir at 'parent directory'.
+if allready exisest, delet all the files inside
+returns 0 on success
+this functin handls the errors so on fail just quit
+argument list:
+parent_directory - char pointer to the directory name */
+int create_empty_dir(char *parent_directory)
+{
+    char path_to_remove[BUFSIZ];
+
+    if (mkdir(parent_directory, 0777) == -1) {
+        if (errno == EEXIST) {
+            DIR *dir = opendir(parent_directory);
+            if (dir == NULL) {
+                fprintf(stderr, "%s:%d: [Error] problem opening '%s': %s\n", __FILE__, __LINE__, parent_directory, strerror(errno));
+                return 1;
+            }
+            struct dirent* entity;
+            entity = readdir(dir);
+            while (entity != NULL) {   /* directory is not empty */
+                strncpy(path_to_remove, parent_directory, BUFSIZ);
+                strncat(path_to_remove, "/", BUFSIZ/2);
+                strncat(path_to_remove, entity->d_name, BUFSIZ);
+                if (entity->d_type == DT_REG) {
+                    if (remove(path_to_remove) != 0) {
+                        fprintf(stderr, "%s:%d: [Error] problem removing '%s': %s\n", __FILE__, __LINE__, path_to_remove, strerror(errno));
+                        return 1;
+                    }
+                }
+                entity = readdir(dir);
+            }
+
+
+            closedir(dir);
+
+            return 0;
+        }
+
+        fprintf(stderr, "%s:%d: [Error] problem making '%s': %s\n", __FILE__, __LINE__, parent_directory, strerror(errno));
+        return 1;
+    }
     return 0;
 }
