0f8f2b3ca39762af72bef848a70d8f29bd3ffeb2
AlmgoDob <dobalmog123@gmail.com>
2025-03-18 20:27:29 +0200

    [code] added some file sorting and middle of IMPLICIT_SW

--- Ex2/code/solver.c
+++ Ex2/code/solver.c
@@ -34,12 +34,13 @@ typedef enum {
 void read_input(char *input_file, int *N, double *x_min, double *x_max, double *L, double *norm_delta_x, double *Re_inf, double *M_inf, double *CFL, double *gamma, double *Pr_inf, double *R_specific, double *T_inf, int *max_iterations, double *final_time, t_flag *flags);
 void output_metadata(char *output_dir, int N, double x_min, double x_max, double Re_inf, double M_inf, double CFL, double gamma, double Pr_inf, double R_specific, double T_inf, int max_iterations, double final_time, t_flag flags);
 void print_mat2D_to_file(FILE *fp, Mat2D m);
+int offset3d(int i, int j, int k, int cols, int rows, int layers);
 double calc_norm_mu(double norm_T, double gamma, double T_inf);
 double calc_norm_kappa(double norm_T, double gamma, double T_inf);
 double calc_norm_energy(double gamma, double norm_rho, double norm_u, double norm_p);
 double calc_norm_p(double gamma, double norm_rho, double norm_u, double norm_e);
 double calc_norm_T(Mat2D Q, double gamma, int i);
-double calc_norm_delta_t(Mat2D Q, double gamma, double norm_delta_x, double CFL, int N);
+double calc_norm_delta_t(Mat2D Q, double gamma, double R_specific, double T_inf, double norm_delta_x, double CFL, int N);
 void calc_T_matrix_at_i(Mat2D T_matrix, Mat2D Q, double gamma, int i);
 void calc_T_inverse_matrix_at_i(Mat2D T_inverse_matrix, Mat2D Q, double gamma, int i);
 void calc_lambda_plus_matrix_at_i(Mat2D lambda_plus_matrix, Mat2D Q, double gamma, int i, double epsilon);
@@ -50,16 +51,18 @@ void calc_vector_of_E(Mat2D E, Mat2D Q, double gamma, int N);
 void calc_vector_of_tilde_norm_E_at_half(Mat2D tilde_norm_E, Mat2D Q, Mat2D work_3_3_mat1, Mat2D work_3_3_mat2, Mat2D work_3_3_mat3, Mat2D work_3_3_mat4, Mat2D work_3_3_mat5, Mat2D work_3_1_mat1, Mat2D work_3_1_mat2, Mat2D work_3_1_mat3, double gamma, double epsilon, int N);
 void calc_vector_of_norm_V1_at_half(Mat2D V1, Mat2D Q, double gamma, double Pr_inf, double T_inf, double norm_delta_x, int N);
 double calc_delta_Q_explicit_steger_warming(Mat2D delta_Q, Mat2D Q, Mat2D work_3_N_1_mat1, Mat2D work_3_N_1_mat2, Mat2D work_3_3_mat1, Mat2D work_3_3_mat2, Mat2D work_3_3_mat3, Mat2D work_3_3_mat4, Mat2D work_3_3_mat5, Mat2D work_3_1_mat1, Mat2D work_3_1_mat2, Mat2D work_3_1_mat3, double gamma, double epsilon, double M_inf, double Re_inf, double Pr_inf, double T_inf, double norm_delta_t, double  norm_delta_x, int N);
+double calc_delta_Q(Mat2D delta_Q, Mat2D Q, Mat2D work_3_N_1_mat1, Mat2D work_3_N_1_mat2, Mat2D work_3_3_mat1, Mat2D work_3_3_mat2, Mat2D work_3_3_mat3, Mat2D work_3_3_mat4, Mat2D work_3_3_mat5, Mat2D work_3_1_mat1, Mat2D work_3_1_mat2, Mat2D work_3_1_mat3, double gamma, double epsilon, double M_inf, double Re_inf, double Pr_inf, double T_inf, double norm_delta_t, double  norm_delta_x, int N, t_flag flags);
+int btri3s(float *a, float *b, float *c, float *f, int kd, int ks, int ke);
 
 int main(int argc, char const *argv[])
 {
 /* declarations */
     char input_file[MAXDIR], init_conditions_file[MAXDIR], output_dir[MAXDIR], temp_word[MAXWORD];
     int N, max_iterations;
-    double x_min, x_max, L, norm_delta_x, Re_inf, M_inf, CFL, gamma, Pr_inf, R_specific, T_inf, final_time, epsilon = 1e-2, norm_delta_t, current_norma;
+    double x_min, x_max, L, norm_delta_x, Re_inf, M_inf, CFL, gamma, Pr_inf, R_specific, T_inf, final_time, epsilon = 1e-2, norm_delta_t, current_norma, elapsed_time = 0, *work_N_by_3_3_array1, *work_N_by_3_3_array2, *work_N_by_3_3_array3;
     t_flag flags = 0;
     Mat2D init_Q, current_Q, next_Q, delta_Q, work_3_N_2_mat1, work_3_N_1_mat1, work_3_N_1_mat2, work_3_3_mat1, work_3_3_mat2, work_3_3_mat3, work_3_3_mat4, work_3_3_mat5, work_3_1_mat1, work_3_1_mat2, work_3_1_mat3;
-    FILE *output_Q_file;
+    FILE *output_Q_file, *output_iter_data_file;
 
 /* getting the input file and output file */
     if (--argc != 3) {
@@ -124,12 +127,28 @@ int main(int argc, char const *argv[])
                 return -1;
             }
         }
+        sprintf(temp_word, "/M%g", M_inf);
+        strcat(output_dir, temp_word);
+        if (create_empty_dir(output_dir) != 0) {
+            fprintf(stderr, "%s:%d: [Error] creating ouput directory\n", __FILE__, __LINE__);
+            return -1;
+        }
+        sprintf(temp_word, "/CFL%g", CFL);
+        strcat(output_dir, temp_word);
+        if (create_empty_dir(output_dir) != 0) {
+            fprintf(stderr, "%s:%d: [Error] creating ouput directory\n", __FILE__, __LINE__);
+            return -1;
+        }
     }
 
     strcpy(temp_word, output_dir);
     strcat(temp_word, "/output_Q.txt");
     output_Q_file = fopen(temp_word, "wt");
 
+    strcpy(temp_word, output_dir);
+    strcat(temp_word, "/output_iter_data.txt");
+    output_iter_data_file = fopen(temp_word, "wt");
+    fprintf(output_iter_data_file, "%s, %s, %s, %s\n", "No", "norma", "norm_delta_time", "elapsed_time");
 
 /*------------------------------------------------------------*/
 /* allocating the matrices */
@@ -162,20 +181,29 @@ int main(int argc, char const *argv[])
     mat2D_copy(next_Q, init_Q);
 
     print_mat2D_to_file(output_Q_file, current_Q);
-    // MAT2D_PRINT(init_Q);
 
 /*------------------------------------------------------------*/
 /* the loop */
-    for (int i = 0; i < 1200; i++) {
+    for (int i = 0; i < 20000; i++) {
         apply_BC(current_Q, N);
-        norm_delta_t = calc_norm_delta_t(current_Q, gamma, norm_delta_x, CFL, N);
 
-        current_norma = calc_delta_Q_explicit_steger_warming(delta_Q, current_Q, work_3_N_1_mat1, work_3_N_1_mat2, work_3_3_mat1, work_3_3_mat2, work_3_3_mat3, work_3_3_mat4, work_3_3_mat5, work_3_1_mat1, work_3_1_mat2, work_3_1_mat3, gamma, epsilon, M_inf, Re_inf, Pr_inf, T_inf, norm_delta_t, norm_delta_x, N);
+        norm_delta_t = calc_norm_delta_t(current_Q, gamma, R_specific, T_inf, norm_delta_x, CFL, N);
+
+        current_norma = calc_delta_Q(delta_Q, current_Q, work_3_N_1_mat1, work_3_N_1_mat2, work_3_3_mat1, work_3_3_mat2, work_3_3_mat3, work_3_3_mat4, work_3_3_mat5, work_3_1_mat1, work_3_1_mat2, work_3_1_mat3, gamma, epsilon, M_inf, Re_inf, Pr_inf, T_inf, norm_delta_t, norm_delta_x, N, flags);
 
         mat2D_add(next_Q, delta_Q);
         mat2D_copy(current_Q, next_Q);
 
         print_mat2D_to_file(output_Q_file, current_Q);
+
+        if (i % 100 == 0) {
+            printf("%d\n",i);
+        }
+        if (0 == i) {
+            fprintf(output_iter_data_file, "%d, %g, %g, %g\n", i+1, current_norma, norm_delta_t, elapsed_time);
+        }
+        elapsed_time += norm_delta_t;
+        fprintf(output_iter_data_file, "%d, %g, %g, %g\n", i+2, current_norma, norm_delta_t, elapsed_time);
     }
 
 /*------------------------------------------------------------*/
@@ -367,6 +395,22 @@ void print_mat2D_to_file(FILE *fp, Mat2D m)
     fprintf(fp, "\n");
 }
 
+/* converts a 3D index into 1D index
+argument list:
+i     - first direction
+j     - second direction
+k     - third direction
+cols  - first direction size
+layer - second direction size */
+int offset3d(int i, int j, int k, int cols, int rows, int layers)
+{
+    assert(i < cols);
+    assert(j < rows);
+    assert(k < layers);
+
+    return (k * layers + i) * cols + j;
+}
+
 double calc_norm_mu(double norm_T, double gamma, double T_inf)
 {
     float constant = 110.4;
@@ -401,9 +445,33 @@ double calc_norm_T(Mat2D Q, double gamma, int i)
     return norm_p / norm_rho;
 }
 
-double calc_norm_delta_t(Mat2D Q, double gamma, double norm_delta_x, double CFL, int N)
+double calc_norm_delta_t(Mat2D Q, double gamma, double R_specific, double T_inf, double norm_delta_x, double CFL, int N)
 {
-    return 1e-4;
+    double delta_t_CFL, delta_t_r, norm_rho, norm_u, norm_e, norm_p, norm_T, norm_a; 
+    double maximum, norm_u_max = 0, norm_T_max = 0;
+
+    for (int i = 1; i <= N; i++) {
+        norm_rho = MAT2D_AT(Q, 0, i);
+        norm_u = MAT2D_AT(Q, 1, i) / norm_rho;
+        norm_e = MAT2D_AT(Q, 2, i);
+        norm_p = calc_norm_p(gamma, norm_rho, norm_u, norm_e);
+        norm_T = norm_p / (norm_rho * R_specific);
+        norm_a = sqrt((gamma * norm_p) / norm_rho);
+
+        maximum = fmax(fabs(norm_u), fmax(fabs(norm_u + norm_a), fabs(norm_u - norm_a)));
+
+        if (maximum > norm_u_max) {
+            norm_u_max = maximum;
+        }
+        if (norm_T > norm_T_max) {
+            norm_T_max = norm_T;
+        }
+    }
+
+    delta_t_CFL = (CFL * norm_delta_x) / norm_u_max;
+    delta_t_r = (norm_delta_x * norm_delta_x) / (2 * calc_norm_mu(norm_T_max, gamma, T_inf));
+
+    return fmin(delta_t_CFL, delta_t_r);
 }
 
 void calc_T_matrix_at_i(Mat2D T_matrix, Mat2D Q, double gamma, int i)
@@ -481,6 +549,11 @@ void calc_lambda_minus_matrix_at_i(Mat2D lambda_minus_matrix, Mat2D Q, double ga
     for (int i = 0; i < 3; i++) {
         MAT2D_AT(lambda_minus_matrix, i, i) = (MAT2D_AT(lambda_minus_matrix, i, i) - sqrt(MAT2D_AT(lambda_minus_matrix, i, i) * MAT2D_AT(lambda_minus_matrix, i, i) + epsilon * epsilon)) / 2;
     }
+}
+
+void calc_norm_P_minus_Rx_matrix_at_i(Mat2D norm_P_minus_Rx)
+{
+    ;
 }
 
 void initialize_Q(char *init_conditions_file, Mat2D Q, double gamma, int N)
@@ -706,3 +779,181 @@ double calc_delta_Q_explicit_steger_warming(Mat2D delta_Q, Mat2D Q, Mat2D work_3
 
     return mat2D_calc_norma(delta_Q);
 }
+
+double calc_delta_Q_implicit_steger_warming(Mat2D delta_Q, Mat2D Q, Mat2D work_3_N_2_mat1, Mat2D work_3_N_1_mat1, Mat2D work_3_N_1_mat2, Mat2D work_3_3_mat1, Mat2D work_3_3_mat2, Mat2D work_3_3_mat3, Mat2D work_3_3_mat4, Mat2D work_3_3_mat5, Mat2D work_3_1_mat1, Mat2D work_3_1_mat2, Mat2D work_3_1_mat3, double *work_N_by_3_3_array1, double *work_N_by_3_3_array2, double *work_N_by_3_3_array3, double gamma, double epsilon, double M_inf, double Re_inf, double Pr_inf, double T_inf, double norm_delta_t, double  norm_delta_x, int N)
+{
+    Mat2D RHS, norm_T, norm_inverse_T, norm_lambda, norm_A, m_temp, vec_temp;
+    double *theta, *phi, *psi;
+
+    RHS            = work_3_N_2_mat1;
+    theta          = work_N_by_3_3_array1;
+    phi            = work_N_by_3_3_array2;
+    psi            = work_N_by_3_3_array3;
+    norm_T         = work_3_3_mat1;
+    norm_inverse_T = work_3_3_mat2;
+    norm_lambda    = work_3_3_mat3;
+    norm_A         = work_3_3_mat4;
+    m_temp         = work_3_3_mat5;
+    vec_temp       = work_3_1_mat1;
+
+    calc_delta_Q_explicit_steger_warming(RHS, Q, work_3_N_1_mat1, work_3_N_1_mat2, work_3_3_mat1, work_3_3_mat2, work_3_3_mat3, work_3_3_mat4, work_3_3_mat5, work_3_1_mat1, work_3_1_mat2, work_3_1_mat3, gamma, epsilon, M_inf, Re_inf, Pr_inf, T_inf, norm_delta_t, norm_delta_x, N);
+
+    int i = 0;
+    /* norm_A_plus */
+    calc_T_matrix_at_i(norm_T, Q, gamma, i);
+    calc_T_inverse_matrix_at_i(norm_inverse_T, Q, gamma, i);
+
+    mat2D_fill(m_temp, 0);
+    mat2D_fill(norm_A, 0);
+    mat2D_fill(vec_temp, 0);
+    calc_lambda_plus_matrix_at_i(norm_lambda, Q, gamma, i, epsilon);
+    mat2D_dot(m_temp, norm_lambda, norm_inverse_T);
+    mat2D_dot(norm_A, norm_T, m_temp);
+}
+
+double calc_delta_Q(Mat2D delta_Q, Mat2D Q, Mat2D work_3_N_1_mat1, Mat2D work_3_N_1_mat2, Mat2D work_3_3_mat1, Mat2D work_3_3_mat2, Mat2D work_3_3_mat3, Mat2D work_3_3_mat4, Mat2D work_3_3_mat5, Mat2D work_3_1_mat1, Mat2D work_3_1_mat2, Mat2D work_3_1_mat3, double gamma, double epsilon, double M_inf, double Re_inf, double Pr_inf, double T_inf, double norm_delta_t, double  norm_delta_x, int N, t_flag flags)
+{
+    double current_norma;
+
+    if (flags & EXPLICIT_SW) {
+        current_norma = calc_delta_Q_explicit_steger_warming(delta_Q, Q, work_3_N_1_mat1, work_3_N_1_mat2, work_3_3_mat1, work_3_3_mat2, work_3_3_mat3, work_3_3_mat4, work_3_3_mat5, work_3_1_mat1, work_3_1_mat2, work_3_1_mat3, gamma, epsilon, M_inf, Re_inf, Pr_inf, T_inf, norm_delta_t, norm_delta_x, N);
+    } else if (flags & IMPLICIT_SW) {
+        ;
+    } else if (flags & EXPLICIT_ROE) {
+        ;
+    }
+    
+    return current_norma;
+}
+
+/* 3x3 block tri-diagonal matrix solver 
+argument list:
+a  - 1D array of 3D matrix 
+b  - 1D array of 3D matrix 
+c  - 1D array of 3D matrix 
+f  - 1D array of 2D matrix 
+kd - N
+ks - 1
+ke - N-2 */
+int btri3s(float *a, float *b, float *c, float *f, int kd, int ks, int ke)
+{
+  /* Local variables */
+  int k, m, n, nd, md;
+
+  float c1, d1, d2, d3, c2, c3, b11, b21, b22, b31, b32, b33, u12, u13, u23;
+  
+  
+  /*   (A,B,C)F = F, F and B are overloaded, solution in F */
+
+  md = 3;
+  nd = 3;
+
+  /*   Part 1. Forward block sweep */
+  
+  for (k = ks; k <= ke; k++)
+    {
+      
+      /*      Step 1. Construct L in B */
+      
+      if (k != ks) 
+	{
+	  for (m = 0; m < md; m++) 
+	    {
+	      for (n = 0; n < nd; n++) 
+		{
+		  b[k + kd * (m + md * n)] = b[k + kd * (m + md * n)] 
+		    - a[k + kd * (m + md * 0)] * b[k - 1 + kd * (0 + md * n)] 
+		    - a[k + kd * (m + md * 1)] * b[k - 1 + kd * (1 + md * n)] 
+		    - a[k + kd * (m + md * 2)] * b[k - 1 + kd * (2 + md * n)] ;
+		}
+	    }
+	}
+      
+      /*      Step 2. Compute L inverse (block matrix) */
+      
+      /*          A. Decompose L into L and U */
+      
+      b11 = 1. / b[k + kd * (0 + md * 0)];
+      u12 = b[k + kd * (0 + md * 1)] * b11;
+      u13 = b[k + kd * (0 + md * 2)] * b11;
+      b21 = b[k + kd * (1 + md * 0)];
+      b22 = 1. / (b[k + kd * (1 + md * 1)] - b21 * u12);
+      u23 = (b[k + kd * (1 + md * 2)] - b21 * u13) * b22;
+      b31 = b[k + kd * (2 + md * 0)];
+      b32 = b[k + kd * (2 + md * 1)] - b31 * u12;
+      b33 = 1. / (b[k + kd * (2 + md * 2)] - b31 * u13 - b32 * u23);
+      
+      /*      Step 3. Solve for intermediate vector */
+      
+      /*          A. Construct RHS */
+      if (k != ks) 
+	{
+	  for (m = 0; m < md; m++) 
+	    {
+	      f[k + kd * m] = f[k + kd * m] 
+		- a[k + kd * (m + md * 0)] * f[k - 1 + kd * 0] 
+		- a[k + kd * (m + md * 1)] * f[k - 1 + kd * 1] 
+		- a[k + kd * (m + md * 2)] * f[k - 1 + kd * 2] ;
+	    }
+	}
+      
+      /*          B. Intermediate vector */
+      
+      /*          Forward substitution */
+      
+      d1 = f[k + kd * 0] * b11;
+      d2 = (f[k + kd * 1] - b21 * d1) * b22;
+      d3 = (f[k + kd * 2] - b31 * d1 - b32 * d2) * b33;
+      
+      /*          Backward substitution */
+      
+      f[k + kd * 2] = d3;
+      f[k + kd * 1] = d2 - u23 * f[k + kd * 2];
+      f[k + kd * 0] = d1 - u12 * f[k + kd * 1] - u13 * f[k + kd * 2];
+      
+      /*      Step 4. Construct U = L ** (-1) * C */
+      /*              by columns and store in B */
+      
+      if (k != ke) 
+	{
+	  for (n = 0; n < nd; n++) 
+	    {
+
+	      /*          Forward substitution */
+	      
+	      c1 = c[k + kd * (0 + md * n)] * b11;
+	      c2 = (c[k + kd * (1 + md * n)] - b21 * c1) * b22;
+	      c3 = (c[k + kd * (2 + md * n)] - b31 * c1 - b32 * c2) * b33;
+	      
+	      /*          Backward substitution */
+	      
+	      b[k + kd * (2 + md * n)] = c3;
+	      b[k + kd * (1 + md * n)] = c2 - u23 * b[k + kd * (2 + md * n)];
+	      b[k + kd * (0 + md * n)] = c1 - u12 * b[k + kd * (1 + md * n)] 
+		- u13 * b[k + kd * (2 + md * n)];
+	    }
+	}
+    }
+  
+  /*   Part 2. Backward block sweep */
+  
+  if (ke == ks) 
+    {
+      return 0;
+    }
+
+  for (k = ke - 1; k >= ks; --k) 
+    {
+      for (m = 0; m < md; m++) 
+	{
+	  f[k + kd * m] = f[k + kd * m] 
+	    - b[k + kd * (m + md * 0)] * f[k + 1 + kd * 0] 
+	    - b[k + kd * (m + md * 1)] * f[k + 1 + kd * 1] 
+	    - b[k + kd * (m + md * 2)] * f[k + 1 + kd * 2] ;
+	}
+    }
+  
+  return 0;
+  
+}
+
